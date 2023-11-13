adf966b836292a0f95179fe84b14cbd9b58abb22
sunyang-629 <sunyang9876@hotmail.com>
2023-11-13 20:42:05 +1000

    03-added a modal for adding new note

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
 import useAsyncStorage from "./src/hooks/use-async-storage";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Modal,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 type NoteType = {
   content: string;
@@ -8,18 +16,36 @@ type NoteType = {
 };
 
 export default function App() {
-  const { storedValue, setValue, removeValue } = useAsyncStorage<NoteType[]>(
-    "notes",
-    []
-  );
+  const [isModalVisible, setModalVisible] = useState(false);
 
-  if (storedValue.length === 0)
+  const {
+    storedValue: notes,
+    setValue,
+    removeValue,
+  } = useAsyncStorage<NoteType[]>("notes", []);
+
+  const handleTogglingModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  if (notes.length === 0)
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Button title="Add New Note" onPress={() => setModalVisible(true)} />
+        </View>
         <Text>
           Uh-oh! No notes found. Time to get creative and jot something down!
         </Text>
-      </View>
+        <Modal visible={isModalVisible} animationType="slide">
+          <View style={styles.modalContainer}>
+            <Text>add a new note</Text>
+            <View style={styles.modalButtonsContainer}>
+              <Button title="Cancel" onPress={handleTogglingModal} />
+            </View>
+          </View>
+        </Modal>
+      </SafeAreaView>
     );
 }
 
@@ -28,6 +54,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    // justifyContent: "center",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 16,
+    // backgroundColor: "#eee",
+    width: "100%",
+  },
+  // addButtonContainer: {
+  //   position: "absolute",
+  //   top: 32,
+  //   right: 16,
+  // },
+  modalContainer: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    // backgroundColor: "red",
+  },
+  modalButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
